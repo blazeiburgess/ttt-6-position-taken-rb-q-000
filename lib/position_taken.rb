680bc90fac1a620985fc52d7def67b2cbf073ba2
thedailyephemera <thedailyephemera@gmail.com>
2016-04-25 17:25:06 -0400

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,8 @@
 # code your #position_taken? method here!
+def position_taken?(board, position)
+	if board[position] != " " && board[position] != nil && board[position] != ""
+		return true
+	else
+		return false
+	end
+end
